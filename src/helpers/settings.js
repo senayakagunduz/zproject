77259cf5539bf6d8b75552c910ca5c651e90f061
senayakagunduz <senayakgndz@gmail.com>
2023-07-11 12:22:10 +0300

    initial commit

---
+++ src/helpers/settings.js
@@ -0,0 +1,10 @@
+export const settings = {
+    apiURL : "https://carrental-v3-backend.herokuapp.com",
+    siteName: "Let's Rental Car",
+    siteUrl : "letsrentalcar.com",
+    email : "support@letsrentalcar.com",
+    phone1: "(845)-356-0642",
+    phone2: "(845)-556-7624",
+    adress:"431 5th Ave, New York, NY 10016, USA",
+    mapEmbedUrl : ""
+}
---
+++ src/templates/user-template.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../components/user/common/header/header";
+import Footer from "../components/user/common/footer/footer";
+
+const UserTemplate = ({children}) => {
+  return (
+    <>
+      <Header />
+      { children }
+      <Footer />
+    </>
+  );
+};
+
+export default UserTemplate;
