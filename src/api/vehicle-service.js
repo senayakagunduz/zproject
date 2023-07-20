1a256799797390f843256c774fd13e93e51ea332
senayakagunduz <senayakgndz@gmail.com>
2023-07-20 04:51:42 +0300

    vehicle page created

---
+++ src/api/vehicle-service.js
@@ -0,0 +1,12 @@
+import axios from "axios"
+import {settings} from "../helpers/settings"
+
+const API_URL= settings.apiURL;
+
+export const getVehicles=()=>{
+    return axios.get(`${API_URL}/car/visitors/all`);
+}
+
+export const getVehiclesByPage=(page=0,size=6,sort="model",direction="ASC")=>{
+   return axios.get(`${API_URL}/car/visitors/pages?page=${page}&size=${size}&sort=${sort}&direction=${direction}`,)
+}
