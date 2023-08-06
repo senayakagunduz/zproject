c5e2dc8260030ba9bebe30d034f65a91859e1001
senayakagunduz <senayakgndz@gmail.com>
2023-08-06 23:38:45 +0300

    admin users edit created

--- src/api/contact-service.js
+++ src/api/contact-service.js
@@ -3,9 +3,11 @@ import {settings} from "../helpers/settings"
 import authHeader from "../helpers/functions/auth-header";
 
 const API_URL= settings.apiURL;
+//USER ENDPOINTS
 export const sendMessage=(message)=>{
     axios.post(`${API_URL}/contactmessage/visitors`,message);
 }
+//ADMIN ENDPOINTS
 export const getMessagesByPage=(page=0,size=20,sort="id",direction="DESC")=>{
     return axios.get(`${API_URL}/contactmessage/pages?page=${page}&size=${size}&sort=${sort}&direction=${direction}`,
     {headers: authHeader(),})
